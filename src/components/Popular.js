43bd91b3b959ba86c2787575d1ace4ffb1745ab9
Angelika Czerwińska <angelikaczerwinska9@gmail.com>
2023-08-05 16:36:51 +0200

    Fetched images and titles for recipes

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,7 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { API_KEY } from "../Routes";
+import "./Popular.css";
 
 function Popular() {
+  const [popular, setPopular] = useState([]);
+
   useEffect(() => {
     getPopular();
   }, []);
@@ -11,10 +14,29 @@ function Popular() {
       `https://api.spoonacular.com/recipes/random?apiKey=ce1e25bd03cc4691830b622b58c81275&number=9`
     );
     const data = await api.json();
-    console.log(data);
+    setPopular(data.recipes);
+    console.log(data.recipes);
   };
 
-  return <div>Popular</div>;
+  return (
+    <div>
+      {popular.map((recipe) => {
+        return (
+          <div className="wrapper" key={recipe.id}>
+            <h3>Popular Picks</h3>
+            {popular.map((recipe) => {
+              return (
+                <div className="card">
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Popular;
