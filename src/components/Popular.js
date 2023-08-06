115df7c92cbd63e81810a32fea124dcc1cc9f59b
Angelika Czerwińska <angelikaczerwinska9@gmail.com>
2023-08-06 14:50:07 +0200

    Created Slider with Splide

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { API_KEY } from "../Routes";
 import "./Popular.css";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
 
 function Popular() {
   const [popular, setPopular] = useState([]);
@@ -10,31 +12,50 @@ function Popular() {
   }, []);
 
   const getPopular = async () => {
-    const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=ce1e25bd03cc4691830b622b58c81275&number=9`
-    );
-    const data = await api.json();
-    setPopular(data.recipes);
-    console.log(data.recipes);
+    const check = localStorage.getItem("popular");
+
+    if (check) {
+      setPopular(JSON.parse(check));
+    } else {
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=ce1e25bd03cc4691830b622b58c81275&number=9`
+      );
+      const data = await api.json();
+      localStorage.setItem("popular", JSON.stringify(data.recipes));
+      setPopular(data.recipes);
+      console.log(data.recipes);
+    }
   };
 
   return (
     <div>
-      {popular.map((recipe) => {
-        return (
-          <div className="wrapper" key={recipe.id}>
-            <h3>Popular Picks</h3>
-            {popular.map((recipe) => {
-              return (
+      <div className="wrapper">
+        <h3>Popular Picks</h3>
+        <Splide
+          options={{
+            perPage: 4,
+            arrows: false,
+            pagination: false,
+            drag: "free",
+            gap: "50px",
+          }}
+        >
+          {popular.map((recipe) => {
+            return (
+              <SplideSlide key={recipe.id}>
                 <div className="card">
                   <p>{recipe.title}</p>
-                  <img src={recipe.image} alt={recipe.title} />
+                  <img
+                    className="card-image"
+                    src={recipe.image}
+                    alt={recipe.title}
+                  />
                 </div>
-              );
-            })}
-          </div>
-        );
-      })}
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </div>
     </div>
   );
 }
